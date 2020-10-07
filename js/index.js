45eaa47305e76c43536634bd0b3b62e77aa750d5
FedericoWalovnik <fwalovnik@gmail.com>
2020-10-07 18:27:42 -0300

    slideshow functionality

--- js/index.js
+++ js/index.js
@@ -18,3 +18,51 @@ const data = [
       'Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.'
   }
 ];
+
+let index = 0;
+
+const updateInfo = () => {
+  title.innerHTML = data[index].title;
+  paragraph.innerHTML = data[index].paragraph;
+  slideshow.style.backgroundImage = `url('/images/desktop-image-hero-${
+    index + 1
+  }.jpg')`;
+};
+
+const updateIndex = (add) => {
+  if (add) {
+    if (index <= 1) {
+      index++;
+    } else {
+      index = 0;
+    }
+  } else {
+    if (index > 0) {
+      index--;
+    } else {
+      index = 2;
+    }
+  }
+};
+
+window.addEventListener('load', function () {
+  const title = document.getElementById('title');
+  const paragraph = document.getElementById('paragraph');
+  const slideshow = document.getElementById('slideshow');
+  console.log(slideshow);
+
+  const next = document.getElementById('next');
+  const previous = document.getElementById('previous');
+
+  next.addEventListener('click', () => {
+    updateIndex(true);
+    console.log(index);
+    updateInfo();
+  });
+
+  previous.addEventListener('click', () => {
+    updateIndex(false);
+    console.log(index);
+    updateInfo();
+  });
+});
